62f88fcc7a7ed1c83c832308a5b1bc07c465aabe
Jeff <jeffrey.arneson@gmail.com>
2021-09-09 19:04:46 -0500

    #12 Generate collision shape if ply node has a StaticBody/CollisionShape child.

--- nodes/ply.gd
+++ nodes/ply.gd
@@ -5,17 +5,19 @@ var PlyMesh = preload("../resources/ply_mesh.gd")
 export(Resource) var ply_mesh = PlyMesh.new()
 export(Material) var material = preload("../debug_material.tres")
 
-
 var mesh_instance = null
 func _enter_tree():
-    render_mesh()
+    if not ply_mesh.is_connected("mesh_updated", self, "_on_mesh_updated"):
+        ply_mesh.connect("mesh_updated", self, "_on_mesh_updated")
+    _on_mesh_updated()
 
-    if not ply_mesh.is_connected("mesh_updated", self, "render_mesh"):
-        ply_mesh.connect("mesh_updated", self, "render_mesh")
-
-func render_mesh():
+func _on_mesh_updated():
     if ply_mesh is PlyMesh:
         self.mesh = ply_mesh.get_mesh()
         set("material/0", material)
     else:
         print("not a PlyMesh: %s" % [ply_mesh])
+
+    var collision_shape = get_node_or_null("StaticBody/CollisionShape")
+    if collision_shape:
+        collision_shape.shape = self.mesh.create_trimesh_shape()
