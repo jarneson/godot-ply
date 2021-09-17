1bdf1ba9578bfb8823040a1166263bc8c589b85a
Jeff <jeffrey.arneson@gmail.com>
2021-09-16 20:20:35 -0500

    update interop

--- interop_node.gd
+++ interop_node.gd
@@ -26,6 +26,9 @@ extends Node
 
 var plugin_dictionary = {}
 
+const NOTIFY_CODE_WORK_STARTED = 1
+const NOTIFY_CODE_WORK_ENDED = 2
+
 func register(name, plugin):
     assert(not plugin_dictionary.has(name), 'Plugin "%s" already registered for interop' % [name])
     plugin_dictionary[name] = plugin
@@ -37,3 +40,15 @@ func deregister(name, plugin):
 
 func get_plugin_or_null(name):
     return plugin_dictionary.get(name)
+
+func _notify_plugins(code, args):
+    for plugin_name in plugin_dictionary:
+        var plugin = plugin_dictionary[plugin_name]
+        if plugin.has_method("_interop_notification"):
+            plugin._interop_notification(code, args)
+
+func start_work(what):
+    _notify_plugins(NOTIFY_CODE_WORK_STARTED, what)
+
+func end_work(what):
+    _notify_plugins(NOTIFY_CODE_WORK_ENDED, what)
